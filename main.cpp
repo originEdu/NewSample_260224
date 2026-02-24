efdf27f8a0c04590bf2e6f317a4e43cac82f6f4b
originEdu <originpark82@gmail.com>
2026-02-24 16:52:32 +0900

    17:00 백업

--- main.cpp
+++ main.cpp
@@ -18,5 +18,36 @@ int main()
 	cin >> Money;
 	cout << Money << endl;
 
+	//배열 스타트
+	int MoneyArray[10] = {0}; // Money
+						 //    [0][1][2][3][4][5][6][7][8][9] Money에서부터 얼마만큼 떨어져있는가
+	MoneyArray[0] = 10; //시작위치에서 0만큼 떨어진 곳에 10 대입
+	MoneyArray[1] = 2;  //시작위치에서 1만큼 떨어진 곳에 2 대입
+
+	//위에 것과 아래 것은 같은 것이다.
+	*(MoneyArray+0) = 100;
+	*(MoneyArray + 1) = 20;
+
+	//반복문 스타트
+	for (int Index = 0; Index < 10; Index++) //관례적으로 Index는 i로 변환함 -> 예전 포트란의 잔해
+	{
+		MoneyArray[Index] = 10;
+	}
+
+	//C++ 11이상에서 ranged for문도 사용한다, 아래 것과 위에 것의 같은 것
+	for (int data : MoneyArray)
+	{
+		data = 10;
+	}
+
+	//4byte 2^32 -> 약 -2G ~ +2G의 숫자 표현가능
+	int MoneyOfGiwon = 61; //메모리 공간은 4바이트 잡고 있다
+	//자료형 변수명 = 초기화 상수; 제일 기본적인 포멧
+	
+	//<<endl의 의미 한줄띔과 동시에 버퍼를 비워라라는 뜻
+	//콘솔 입출력장치가 느리기에 "HelloWorld"를 출력할 때
+	//cpu가 노는 것을 방지하기 위해 buffer에 임시로 저장하고 나중에 한번에 출력하는데
+	//endl는 버퍼를 비우고 출력하는 의미도 내포되어 있다.
+
 	return 0;
 }
